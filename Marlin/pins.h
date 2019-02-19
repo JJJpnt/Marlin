6f9c14fdc4a3b8027aeac93319c9a659524ca118
Jean-Julien Jupinet <jean.julien.jupinet@gmail.com>
2019-02-19 18:58:52 +0100

    Fystec_F6 def

--- Marlin/pins.h
+++ Marlin/pins.h
@@ -264,6 +264,9 @@
 #elif MB(5DPRINT)
   #include "pins_5DPRINT.h"           // AT90USB1286                                ?env:at90USB1286_DFU
 
+#elif MB(FYSETC_F6_13)
+  #include "pins_FYSETC_F6_13.h"
+
 #else
   #error "Unknown MOTHERBOARD value set in Configuration.h"
 #endif
